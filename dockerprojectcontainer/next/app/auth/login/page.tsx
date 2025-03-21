bab34ea33fc7eb3bfa06bb472098fc71ba7c8a68
adamcanard <azcunard@gmail.com>
2025-03-21 15:27:05 -0600

    added delete user

--- dockerprojectcontainer/next/app/auth/login/page.tsx
+++ dockerprojectcontainer/next/app/auth/login/page.tsx
@@ -5,13 +5,18 @@ import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Page() {
   const [userId, setUserId] = useState<number>(0);
+  const [deleteUserId, setDeleteUserId] = useState<number>(0);
   const [password, setPassword] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
   const router = useRouter();
 
   const addUserId = (increase: number) => {
-    console.log(increase);
     const newUserId = userId + increase;
     setUserId(newUserId);
+  };
+  const addDeleteUserId = (increase: number) => {
+    const newUserId = deleteUserId + increase;
+    setDeleteUserId(newUserId);
   };
   const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
@@ -34,10 +39,38 @@ export default function Page() {
       console.log(e);
     }
   };
+  const deleteUser = async () => {
+    try {
+      const response = await fetch("/api/user", {
+        method: "DELETE",
+        body: JSON.stringify({ userId: deleteUserId }),
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        setMessage(data.message);
+        console.log(response);
+      } else {
+        const data = await response.json();
+        setMessage(data.message);
+        console.log(response);
+      }
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        setMessage(e.message);
+      }
+    }
+  };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     await getUsers();
+  };
+
+  const handleDelete = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await deleteUser();
   };
 
   return (
@@ -107,6 +140,58 @@ export default function Page() {
             <button>Submit</button>
           </div>
         </form>
+      </div>
+
+      <div className={"Box"}>
+        {message !== "" ? (
+          <div className={"select-none"} onClick={() => setMessage("")}>
+            {message}
+          </div>
+        ) : (
+          <>
+            <h1>Delete an account</h1>
+            <form
+              className={"w-72 flex flex-col gap-2"}
+              onSubmit={(e) => handleDelete(e)}
+            >
+              <div className={"w-full flex justify-between flex-col"}>
+                <label>UserId:</label>
+                <div className={"flex flex-row justify-around w-full"}>
+                  <input
+                    disabled
+                    className={"w-full text-center"}
+                    name="userId"
+                    value={deleteUserId}
+                  />
+                  <div className={"flex w-full justify-around"}>
+                    <button
+                      type="button"
+                      onClick={() => addDeleteUserId(1)}
+                      className={"w-10"}
+                    >
+                      +1
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => addDeleteUserId(5)}
+                      className={"w-10"}
+                    >
+                      +5
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => addDeleteUserId(10)}
+                      className={"w-10"}
+                    >
+                      +10
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <button>Delete</button>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
