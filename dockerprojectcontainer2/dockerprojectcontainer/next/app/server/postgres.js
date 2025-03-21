b249e4ea278f3965c0c2f2a9a87fa16a5b0d941b
root <root@adamcanard.adamcanard>
2025-03-21 17:23:59 -0600

    what

--- dockerprojectcontainer2/dockerprojectcontainer/next/app/server/postgres.js
+++
@@ -1,8 +0,0 @@
-import pg from "pg";
-const { Pool } = pg;
-export const client = new Pool({
-  host: "postgresql",
-  port: 5432,
-  user: "postgres",
-  password: "P@ssw0rd",
-});
--- dockerprojectcontainer2/next/app/server/postgres.js
+++ dockerprojectcontainer2/next/app/server/postgres.js
@@ -2,7 +2,7 @@ import pg from "pg";
 const { Pool } = pg;
 export const client = new Pool({
   host: "postgresql",
-  port: 5432,
+  port: 5433,
   user: "postgres",
   password: "P@ssw0rd",
 });
